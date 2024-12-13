5e51c78d8bf5e8d30eef31342354ad30837bea55
Md. Al-Amin <102671792+Md-Alamin-BS@users.noreply.github.com>
2024-12-13 18:26:55 +0600

    start_pandas

--- pandas/pandas_test.py
+++ pandas/pandas_test.py
@@ -7,4 +7,9 @@ my_series = pd.Series(my_list)
 # print(my_series)
 s = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-print(my_series[4])
+# print(my_series[4])
+
+my_index = ['A', 'B', 'C', 'D', 'E']
+my_series2 = pd.Series(my_list, my_index)
+my_series3 = pd.Series(my_list, index=my_index)
+print(my_series2)
